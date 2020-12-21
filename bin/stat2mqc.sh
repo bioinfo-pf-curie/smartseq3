ffc68f062d8e7460548130950a01c1962d28a335
LouisaHadj <louisa.hadj-abed@curie.fr>
2020-12-21 11:16:42 +0100

    correct table in mqc and change the test data

--- bin/stat2mqc.sh
+++ bin/stat2mqc.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 splan=$1
-#filtreUmi1=$3
-#filtreUmi2=$5
-#filtreGene1=$7
-#filtreGene2=$9
 
 ## Catch sample names
 all_samples=$(awk -F, '{print $1}' $splan)
-
 
 ## Header
 #Table:
 echo -e "Sample_id, Sample_name, Reads tot., UMIs(%), Aligned(%), Assigned(%), #Genes, #UMIs" > table_mqc.stats
 #Bargraph:
-echo -e "Sample_id, Sample_name, Aligned_Assigned, Aligned_NotAssigned, NotAligned, NotAligned_NotAssagned" > final_mqc.stats
+echo -e "Sample_id, Sample_name, Aligned_Assigned, Aligned_NotAssigned, NotAligned_NotAssagned" > final_mqc.stats
 
 for sample in $all_samples
 do
@@ -27,10 +22,8 @@ do
     aligned=` grep "Uniquely mapped reads number" star/${sample}Log.final.out | cut -d'|' -f2 `
     paligned=` grep "Uniquely mapped reads %" star/${sample}Log.final.out | cut -d'|' -f2 | cut -d% -f1`
     aligned_assigned=`grep "Assigned" FC/${sample}_counts.summary | cut -f2`
-    # Number of assigned across barcoded and aligned
     NotAligned=`echo $(( $tot_reads - $aligned ))`
     aligned_NotAssigned=`echo $(( $aligned - $aligned_assigned ))`
-    NotAligned_NotAssigned=`echo $(( $tot_reads - $aligned_assigned ))`
 
     if [ $aligned_assigned != 0 ]; then  
         paligned_assigned=$(echo "scale=2; ($aligned_assigned*100/$tot_reads)" | bc -l)
@@ -41,8 +34,8 @@ do
     nbGenes=$(grep ${sample} resume.txt | cut -d, -f3)
     nbUMIs=$(grep ${sample} resume.txt | cut -d, -f2)
 
-    echo -e ${sample},${sname},${tot_reads}, ${pUMIs}, ${paligned}, ${paligned_assigned},${nbGenes}, ${nbUMIs}>> table_mqc.stats
-    echo -e ${sample},${sname},${aligned_assigned}, ${aligned_NotAssigned},${NotAligned},${NotAligned_NotAssigned} >> final_mqc.stats
+    echo -e ${sample},${sname},${tot_reads}, ${pUMIs}, ${paligned}, ${paligned_assigned},${nbGenes}, ${nbUMIs} >> table_mqc.stats
+    echo -e ${sample},${sname},${aligned_assigned}, ${aligned_NotAssigned},${NotAligned} >> final_mqc.stats
 
 done
 
