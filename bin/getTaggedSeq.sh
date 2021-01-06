afd970fab2377ddbd7d5bfad45703071c6206993
LouisaHadj <louisa.hadj-abed@curie.fr>
2021-01-06 16:30:17 +0100

    correct get tagged seq to extract even tagges in R2

---
+++ bin/getTaggedSeq.sh
@@ -0,0 +1,14 @@
+
+
+fastq1=$1
+fastq2=$2
+out1=$3
+out2=$4
+out3=$5
+
+
+seqkit grep --by-seq --pattern "ATTGCGCAATG" $fastq1 > $out1
+# exctract ids
+seqkit seq -n -i $out1 > $out2
+# create R2
+seqkit grep -f $out2 $fastq2 -o $out3
