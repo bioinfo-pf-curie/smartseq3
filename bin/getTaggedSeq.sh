70371f69a4387c0ad2b9d8828d75fc437cb88602
LouisaHadj <louisa.hadj-abed@curie.fr>
2021-01-06 17:19:33 +0100

    test adding R2 tags

--- bin/getTaggedSeq.sh
+++ bin/getTaggedSeq.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-fastq_in1=$1
-fastq_in2=$2
-fastq_out1=$3
-txt_out2=$4
-fastq_out3=$5
 
 # get tagged sequences
-seqkit grep --by-seq --pattern "ATTGCGCAATG" $fastq_in1 > $fastq_out1
-echo $fastq_out1
-head $fastq_out1
+seqkit grep --by-seq --pattern "ATTGCGCAATG" $1 > $3
+echo $3
+head $3
 # extract ids
-seqkit seq -n -i $fastq_out1 > $txt_out2
-echo $txt_out2
-head $txt_out2
+seqkit seq -n -i $3 > $4
+echo $4
+head $4
 # get paired reads
-seqkit grep -f $txt_out2 $fastq_in2 -o $fastq_out3
-echo $fastq_out3
-head $fastq_out3
+seqkit grep -f $4 $2 -o $5
+echo $5
+head $5
