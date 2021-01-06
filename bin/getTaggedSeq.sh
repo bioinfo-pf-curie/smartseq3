8cdfd89e69e3d098c3934edfff3f12f99728f6b7
LouisaHadj <louisa.hadj-abed@curie.fr>
2021-01-06 17:25:34 +0100

    test adding R2 tags
    Former-commit-id: 9b18d0e5bd0de6c7650c383997d0ccd21b89f4f2

--- bin/getTaggedSeq.sh
+++ bin/getTaggedSeq.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+echo $2
+head $2
+
+echo $3
+head $3
+
+echo $4
+head $4
+
+echo $5
+head $5
+
+echo $6
+head $6
 
 # get tagged sequences
 seqkit grep --by-seq --pattern "ATTGCGCAATG" $2 > $4
-echo $4
-head $4
 # extract ids
 seqkit seq -n -i $4 > $5
-echo $5
-head $5
 # get paired reads
 seqkit grep -f $5 $3 -o $6
-echo $6
-head $6
