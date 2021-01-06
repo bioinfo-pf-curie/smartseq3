b705870af6bd764e42ac402740f6ec4ac35a92dc
LouisaHadj <louisa.hadj-abed@curie.fr>
2021-01-06 17:21:50 +0100

    test adding R2 tags

--- bin/getTaggedSeq.sh
+++ bin/getTaggedSeq.sh
@@ -2,14 +2,14 @@
 
 
 # get tagged sequences
-seqkit grep --by-seq --pattern "ATTGCGCAATG" $1 > $3
-echo $3
-head $3
-# extract ids
-seqkit seq -n -i $3 > $4
+seqkit grep --by-seq --pattern "ATTGCGCAATG" $2 > $4
 echo $4
 head $4
-# get paired reads
-seqkit grep -f $4 $2 -o $5
+# extract ids
+seqkit seq -n -i $4 > $5
 echo $5
 head $5
+# get paired reads
+seqkit grep -f $5 $3 -o $6
+echo $6
+head $6
